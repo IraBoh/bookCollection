4f47aac81afd2f80e870487bb4ca616fd36e58c4
Iryna <irynabohdan2101@gmail.com>
2024-08-20 15:37:26 +0200

    BookController: Get,Post,Put,Delete

--- bookcollection/src/main/java/com/example/bookcollection/interfaceL/BookController.java
+++ bookcollection/src/main/java/com/example/bookcollection/interfaceL/BookController.java
@@ -1,11 +1,15 @@
 package com.example.bookcollection.interfaceL;
 
 import com.example.bookcollection.application.BookService;
+import com.example.bookcollection.domain.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -14,8 +18,37 @@ public class BookController {
     private final BookService bookService;
 
     @Autowired
-    public BookController(BookService bookService){
-        this.bookService=bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Book>> getBook(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Book>> searchBooks(@RequestParam(required = false) String title, @RequestParam(required = false)
+                                                  String author){
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> createBook(@RequestBody Book book){
+        bookService.createBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateBook(@PathVariable Long id, @RequestBody Book updatedBook){
+        bookService.updateBook(id,updatedBook);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id){
+        bookService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
