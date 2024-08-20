0e46f014c7ad02b46bfe367fe58b7819d696752b
Iryna <irynabohdan2101@gmail.com>
2024-08-20 20:51:49 +0200

    BookRepository:fix

--- bookcollection/src/main/java/com/example/bookcollection/domain/repository/BookRepository.java
+++ bookcollection/src/main/java/com/example/bookcollection/domain/repository/BookRepository.java
@@ -1,12 +1,14 @@
 package com.example.bookcollection.domain.repository;
 
 import com.example.bookcollection.domain.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository{
+
 
     List<Book> findAll();
     Optional<Book> findById(Long id);
