7345566e7560b89e76c1a9958338230889d04ac5
Iryna <irynabohdan2101@gmail.com>
2024-08-20 20:53:23 +0200

    BookService: filter with lambda

--- bookcollection/src/main/java/com/example/bookcollection/application/BookService.java
+++ bookcollection/src/main/java/com/example/bookcollection/application/BookService.java
@@ -4,9 +4,11 @@ import com.example.bookcollection.domain.model.Book;
 import com.example.bookcollection.domain.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -24,6 +26,12 @@ public class BookService {
     public Optional<Book> getBookById(Long id){
         return bookRepository.findById(id);
     }
+    public List<Book> getBookByAuthor(String author){
+        return bookRepository.findAll()
+                .stream().filter(book ->
+                        book.getAuthor().equals(author)).collect(Collectors.toList());
+    }
+
     public void createBook(Book book){
         bookRepository.save(book);
     }
@@ -32,6 +40,7 @@ public class BookService {
         bookRepository.save(updatedBook);
     }
 
+    @Transactional
     public void deleteById(Long id){
         bookRepository.deleteById(id);
     }
