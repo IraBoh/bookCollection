4dc1a10e5a0d28f266595f28cc2e2dd26c6a49b2
Iryna <irynabohdan2101@gmail.com>
2024-08-20 20:51:11 +0200

    BookService:/searchAll, /searchAuthor added

--- bookcollection/src/main/java/com/example/bookcollection/interfaceL/BookController.java
+++ bookcollection/src/main/java/com/example/bookcollection/interfaceL/BookController.java
@@ -2,6 +2,8 @@ package com.example.bookcollection.interfaceL;
 
 import com.example.bookcollection.application.BookService;
 import com.example.bookcollection.domain.model.Book;
+import jakarta.annotation.security.RolesAllowed;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//@RolesAllowed({"sa"})
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -22,33 +25,49 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Book>> getBook(@PathVariable Long id) {
+    @GetMapping
+    public String start(){
+        return ("Welcome to the Start Page!");
+    }
+
+    @GetMapping("/searchId/{id}")
+    public ResponseEntity<Optional<Book>> getBookByIdPathParam(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBookById(id));
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<Book>> searchBooks(@RequestParam(required = false) String title, @RequestParam(required = false)
-                                                  String author){
+    @GetMapping("/searchId")
+    public ResponseEntity<Optional<Book>> getBookByIdQueryParam(@RequestParam(required = true) Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    @GetMapping("/searchAll")
+    public ResponseEntity<List<Book>> searchAllBooks(){
         List<Book> books = bookService.getAllBooks();
         return ResponseEntity.ok(books);
     }
 
-    @PostMapping("/")
+    @GetMapping("/searchAuthor")
+    public ResponseEntity<List<Book>> searchBooksByAuthor(@RequestParam(required = true)
+    String author){
+        List<Book> books = bookService.getBookByAuthor(author);
+        return ResponseEntity.ok(books);
+    }
+
+    @PostMapping("/new")
     public ResponseEntity<Void> createBook(@RequestBody Book book){
         bookService.createBook(book);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateBook(@PathVariable Long id, @RequestBody Book updatedBook){
+    @PutMapping("/update")
+    public ResponseEntity<Void> updateBook(@RequestParam(required = true) Long id, @RequestBody Book updatedBook){
         bookService.updateBook(id,updatedBook);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBook(@PathVariable Long id){
-        bookService.deleteById(id);
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteBook(@RequestParam(required = true) Long id){
+        System.out.println("DELETE request received for id: " + id); bookService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
